6280b96aec031ae3585faf9dd9c5c0c051bde67a
naz_liton <nazrulliton@icloud.com>
2019-10-21 13:39:27 +0100

    getters and setters

---
+++ lab_day1/lab.rb
@@ -0,0 +1,40 @@
+class Student
+
+def initialize (name, cohort)
+@name = name
+@cohort = cohort
+end
+
+def get_name
+return @name
+end
+
+def get_cohort
+  return @cohort
+end
+
+def set_name (name)
+  return @name = name
+end
+
+def set_cohort (cohort)
+  return @cohort = cohort
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ lab_day1/specs/class_spec.rb
@@ -0,0 +1,45 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../lab.rb')
+
+
+class TestStudent < MiniTest::Test
+
+def test_student_name
+   student = Student.new("Jeff", "E30")
+  assert_equal("Jeff", student.get_name)
+end
+
+def test_student_cohort
+   student = Student.new("Jeff", "E30")
+  assert_equal("E30", student.get_cohort)
+end
+
+def test_set_student_name
+  student = Student.new("Jeff", "E30")
+student.set_name("John")
+assert_equal("John",student.get_name)
+end
+
+def test_set_student_cohort
+  student = Student.new("Jeff", "E30")
+student.set_cohort("D32")
+assert_equal("D32",student.get_cohort)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
